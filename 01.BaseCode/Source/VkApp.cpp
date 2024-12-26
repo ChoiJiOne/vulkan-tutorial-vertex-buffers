c5fb51e5ed65178dacd5a48b4163afd7b8e9d45d
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-12-26 15:57:26 +0900

    01.BaseCode 실습 코드 추가

--- 01.BaseCode/Source/VkApp.cpp
+++ 01.BaseCode/Source/VkApp.cpp
@@ -2,6 +2,20 @@
 
 void VkApp::run()
 {
+	initWindow();
+	initVulkan();
+	mainLoop();
+	cleanup();
+}
+
+void VkApp::initWindow()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Tutorial", nullptr, nullptr);
 }
 
 void VkApp::initVulkan()
@@ -10,8 +24,16 @@ void VkApp::initVulkan()
 
 void VkApp::mainLoop()
 {
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
 }
 
 void VkApp::cleanup()
 {
+	glfwDestroyWindow(window);
+	window = nullptr;
+
+	glfwTerminate();
 }
--- 01.BaseCode/Source/VkApp.h
+++ 01.BaseCode/Source/VkApp.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+#include <glfw/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+
+const uint32_t WIDTH = 1000;
+const uint32_t HEIGHT = 800;
 
 class VkApp
 {
@@ -12,7 +16,11 @@ public:
 	void run();
 
 private:
+	void initWindow();
 	void initVulkan();
 	void mainLoop();
 	void cleanup();
+
+private:
+	GLFWwindow* window;
 };
