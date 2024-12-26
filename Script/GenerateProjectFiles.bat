27b6c8eace8762d25da512e046ef245dba4f7369
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-12-26 15:05:50 +0900

    Visual Studio 솔루션 및 하위 프로젝트 파일을 생성하는 스크립트 추가

---
+++ Script/GenerateProjectFiles.bat
@@ -0,0 +1,74 @@
+@ECHO OFF
+
+ECHO =============================
+ECHO Start Generate Project Files
+ECHO =============================
+
+@ECHO OFF
+where cmake > nul 2>&1
+if %ERRORLEVEL% EQU 0 (
+    ECHO CMake is installed on this system...
+) else (
+    ECHO CMake is not installed on this system...
+    GOTO:EOF
+)
+
+SET VISUAL_STUDIO="Visual Studio 17 2022"
+
+SET PROJECT_NAME=%1
+if "%PROJECT_NAME%" == "" (
+    ECHO Project name is empty...
+    GOTO:EOF
+) else (
+    ECHO Project name is "%PROJECT_NAME%"...
+)
+
+SET RUN_OPTION=%2
+if "%RUN_OPTION%" == "on" (
+    SET RUN="on"
+) else if "%RUN_OPTION%" == "On" (
+    SET RUN="on"
+) else if "%RUN_OPTION%" == "off" (
+    SET RUN="off"
+) else if "%RUN_OPTION%" == "Off" (
+    SET RUN="off"
+) else (
+    SET RUN="off"
+)
+ECHO Switch %RUN% run Visual Studio Solution...
+
+SET SOLUTION_PATH=%~dp0..\Solution
+
+if not exist "%SOLUTION_PATH%" (
+    mkdir "%SOLUTION_PATH%"
+)
+
+if not exist "%~dp0..\Cache" (
+    mkdir "%~dp0..\Cache"
+)
+
+if not exist "Game\\Inc" (
+    mkdir "Game\\Inc"
+)
+
+if not exist "Game\\Src" (
+    mkdir "Game\\Src"
+)
+
+SET CURRENT_PATH=%~dp0
+
+PUSHD "%CURRENT_PATH%"
+PUSHD "%SOLUTION_PATH%"
+
+cmake .. -G %VISUAL_STUDIO% -A "x64"
+
+if %RUN% == "on" (
+    start %PROJECT_NAME%.sln
+)
+
+POPD
+POPD
+
+ECHO =============================
+ECHO Done Generate Project Files
+ECHO =============================
