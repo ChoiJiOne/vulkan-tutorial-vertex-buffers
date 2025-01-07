027b4fe39b18e99a894d10f97f2ea37fa0703661
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-01-07 18:35:33 +0900

    전체 폴더 이름 변경

---
+++ 1.Drawing a triangle/1.5.Swap chain recreation/Source/Utils.cpp
@@ -0,0 +1,20 @@
+#include "Utils.h"
+
+std::vector<char> readFile(const std::string& filename)
+{
+	std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+	if (!file.is_open())
+	{
+		throw std::runtime_error("failed to open file!");
+	}
+
+	std::size_t fileSize = static_cast<std::size_t>(file.tellg());
+	std::vector<char> buffer(fileSize);
+
+	file.seekg(0);
+	file.read(buffer.data(), fileSize);
+	file.close();
+
+	return buffer;
+}
